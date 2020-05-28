7eeca874dc8c2ac8016004807d3da6d53da77923
Eric Zong <ericzonglu@126.com>
2020-05-28 19:29:37 +0800

    更新发布脚本

--- dist.ps1
+++ dist.ps1
@@ -4,25 +4,46 @@ param(
   [string]$compilerRoot
 )
 
+Trap {
+  "Get Exception: $($_.Exception.Message)"
+  cd $PSScriptRoot
+}
+
+$ErrorActionPreference='Stop'
+
+cd $PSScriptRoot
 # ¿½±´ÎÄ¼þ
-$files = './autorun', './config', './hotkey.png','.\hotkey_pause.png', '.\hotkey_suspend.png', '.\README.md', '.\RELEASES.md', '.\LICENSE'
-foreach($f in $files) {
-  if(Test-Path $f) {
-    copy (Resolve-Path $f) .\dist -Force -Recurse
+$files = 'autorun', 'config', 'hotkey.png','hotkey_pause.png', 'hotkey_suspend.png', 'README.md', 'RELEASES.md', 'LICENSE'
+switch($files) {
+  {Test-Path $_} {
+    copy $_ dist -Force -Recurse
+    Write-Host "Copy $_ ..."
   }
 }
 
-$script = '.\Key++.ahk'
-$outExe = '.\dist\Key++.exe'
-$icon = '.\ico\Key++.ico'
+$script = (Join-Path $PSScriptRoot 'Key++.ahk')
+$outExe = [IO.Path]::Combine($PSScriptRoot, 'dist', 'Key++.exe')
+$icon = [IO.Path]::Combine($PSScriptRoot, 'ico', 'Key++.ico')
+$distDir = (Join-Path $PSScriptRoot 'dist')
 $bin32 = (Join-Path $compilerRoot 'Unicode 32-bit.bin')
 $bin64 = (Join-Path $compilerRoot 'Unicode 64-bit.bin')
-$zip32 = (Join-Path $PSScriptRoot 'Key++_win32.zip')
-$zip64 = (Join-Path $PSScriptRoot 'Key++_win64.zip')
-$zipFiles = $files, $outExe
+$zip32 = 'Key++_win32.zip'
+$zip64 = 'Key++_win64.zip'
+$zipFiles = $files + $outExe
 
+cd $distDir
+if(-not (Test-Path $files[0])) {
+  mkdir $files[0]
+}
+
+cd $PSScriptRoot
 ahk2exe /in $script /out $outExe /icon $icon /bin $bin32 /mpress 1
+cd $distDir
 7z a $zip32 $zipFiles
 
+cd $PSScriptRoot
 ahk2exe /in $script /out $outExe /icon $icon /bin $bin64 /mpress 1
+cd $distDir
 7z a $zip64 $zipFiles
+
+cd $PSScriptRoot
