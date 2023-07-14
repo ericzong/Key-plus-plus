d77003a5785900b6f105ad4e6bdf48085ac9fcf6
Eric Zong <ericzonglu@126.com>
2023-07-14 18:24:56 +0800

    编译脚本修复；添加编译工具

--- dist.ps1
+++ dist.ps1
@@ -1,8 +1,5 @@
 [CmdletBinding()]
-param(
-  [Parameter(Position=0, Mandatory)]
-  [string]$compilerRoot
-)
+param()
 
 Trap {
   "Get Exception: $($_.Exception.Message)"
@@ -12,11 +9,12 @@ Trap {
 $ErrorActionPreference='Stop'
 
 cd $PSScriptRoot
+
 # 拷贝文件
-$files = 'autorun', 'config', 'hotkey.png','hotkey_pause.png', 'hotkey_suspend.png', 'README.md', 'RELEASES.md', 'LICENSE'
+$files = 'autorun', 'config', 'ico', 'README.md', 'RELEASES.md', 'LICENSE'
 switch($files) {
   {Test-Path $_} {
-    copy $_ dist -Force -Recurse
+    Copy-Item $_ dist -Force -Recurse
     Write-Host "Copy $_ ..."
   }
 }
@@ -26,34 +24,24 @@ $script = (Join-Path $PSScriptRoot 'Key++.ahk')
 $outExe = [IO.Path]::Combine($PSScriptRoot, 'dist', 'Key++.exe')
 $icon = [IO.Path]::Combine($PSScriptRoot, 'ico', 'Key++.ico')
 $distDir = (Join-Path $PSScriptRoot 'dist')
-$bin32 = (Join-Path $compilerRoot 'Unicode 32-bit.bin')
-$bin64 = (Join-Path $compilerRoot 'Unicode 64-bit.bin')
+$compilerRoot = (Join-Path $PSScriptRoot 'compiler')
+$bin32 = (Join-Path $compilerRoot 'AutoHotkey32.exe')
+$bin64 = (Join-Path $compilerRoot 'AutoHotkey64.exe')
 $zip32 = 'Key++_win32.zip'
 $zip64 = 'Key++_x64.zip'
 $zipFiles = $files + $outExe
 
-# FAQ.01 数组中的第一个文件夹(这里是 autorun)由于某种原因可能拷贝失败
-# 如果拷贝失败，就创建一个
-cd $distDir
-if(-not (Test-Path $files[0])) {
-  mkdir $files[0]
-}
-
-cd $PSScriptRoot
-ahk2exe /in $script /out $outExe /icon $icon /bin $bin32 #/mpress 1
-Start-Sleep -s 5
+cd $compilerRoot
+./Ahk2Exe.exe /in $script /out $outExe /icon $icon /base $bin32 /compress 2 | Out-Null
 
 cd $distDir
-7z a $zip32 $zipFiles
-Start-Sleep -s 5
+7z a $zip32 $zipFiles | Out-Null
 
-cd $PSScriptRoot
-ahk2exe /in $script /out $outExe /icon $icon /bin $bin64 #/mpress 1
-Start-Sleep -s 5
+cd $compilerRoot
+./Ahk2Exe.exe /in $script /out $outExe /icon $icon /bin $bin64 /compress 2 | Out-Null
 
 cd $distDir
-7z a $zip64 $zipFiles
-Start-Sleep -s 5
+7z a $zip64 $zipFiles | Out-Null
 
 # MD5, SHA1, SHA256
 $sha256_32 = (certutil -hashfile $zip32 SHA256)[1]
